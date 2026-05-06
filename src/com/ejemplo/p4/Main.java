f937ac5e056ce924dc9b925aa98ac1d7e5349e5e
Minxito <alerumart0701@gmail.com>
2026-05-06 22:54:37 +0200

    Inicio proyecto Practica 4

---
+++ src/com/ejemplo/p4/Main.java
@@ -0,0 +1,7 @@
+package com.ejemplo.p4;
+public class Main {	
+	public static void main(String[] args) {
+		System.out.println("Practica 4 - Git casos reales");
+		System.out.println("Alejandro Ruiz Martínez");
+ }
+}
